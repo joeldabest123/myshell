dc7edaeb7f7b2e741688c5c21db84474587e7c4a
Joel Anderson <joeleverett@hotmail.com>
2026-04-01 17:02:15 -0400

    set up mysh.c a lil

--- mysh.c
+++ mysh.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
 
+int main (int argc, char* argv[]) {
+    if(argc > 2 ) {
+        printf("Usage: %s <directory_path>\n", argv[0]);
+        return 1;
+    }
+
+    if (argc == 1) {
+        //determines if read from std input or argument
+    }
+}
